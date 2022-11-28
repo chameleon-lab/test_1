902f0167748433649a4da0639e4f232106378883
Alice <cpython.projects@gmail.com>
2022-11-28 10:57:17 +0300

    git-playground-conflict

---
+++ main.py
@@ -0,0 +1,39 @@
+import words_fetcher
+import random
+
+
+def congratulate_user():
+    print("=============================")
+    print("= Congratulations! You won! =")
+    print("=============================")
+
+
+def is_game_over():
+    return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
+
+
+guessed = 0
+errors = 0
+
+WORDS_TO_WIN = 5
+ERRORS_TO_LOSE = 3
+
+words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
+full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
+word = words[random.randrange(0, len(words))]
+
+print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
+print(f"Your word is '{word}'")
+
+
+while not is_game_over():
+    guess = input("Your next take: ")
+    if guess in full_list:
+        guessed += 1
+        if guessed == WORDS_TO_WIN:
+            congratulate_user()
+            exit()
+        print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+    else:
+        errors += 1
+        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
